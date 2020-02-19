8f9fb03e4b392a7b011665d4a1983b73464c480f
kruty Tom <kruty.tomas@gmail.com>
2020-02-19 20:27:40 +0100

    first commit

---
+++ src/main/scala/com/smaki/smartkitchen/Main.scala
@@ -0,0 +1,9 @@
+package com.smaki.smartkitchen
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    SmartkitchenServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
